25bdf796ef03c531c864d6a5a984e6a20902a917
misterwolf <dario.gas@gmail.com>
2017-10-19 23:58:01 +0200

    one dependendy

--- lib/jobs_stack.rb
+++ lib/jobs_stack.rb
@@ -1,6 +1,8 @@
 class JobsStack
 
   def initialize(jobs)
+    @queue = []
+    @jobs_with_dependency = []
     # jobs is a string.
     # The string will be parsed (I suppose) because will contain a sort of json
     # so, either jobs.parse
@@ -18,20 +20,121 @@ class JobsStack
     # regular expression for not dependent job
     # ^ => just one not digit letter. Just one is not specified.
     # $ => the string end there 'a =>' => ok, 'a => ' NO ok!
-    @queue = []
-    jobs = jobs.gsub('\n',"\n") # I really don't like that user have to put "" instead of '' to specific the \n
 
+    # -----
+    jobs.gsub!('\n',"\n") # I really don't like that user have to put "" instead of '' to specific the \n
+    jobs.gsub!(" ",'') # it's more confortable check pair with Regular Expression
     # grouping the jobs by their format
     # (\D) => the first job
     # => is dismessed by the group.
-    # (:?\D|) => something different from number (a \n or a letter) or nothing: ('=>\n', '=>' or '=>b')
-    jobs.scan(/(\D) =>(:?\D|)/).each{ |job|
+    # (:?\w|) => will match only letters or nothing: ('=>\n' -> '', '=>b' 'b')
+    jobs.scan(/(\D)=>(:?\w|)/).each{ |job|
       @queue << job.first
+      @jobs_with_dependency << job
     }
+    sort_with_no_significant_order
+    sort_jobs_with_dependency
   end
 
   def queue
-    @queue.sort {|a,b| a<=>b} # sort alphabetically
+    @queue
+  end
+
+  def sorted_jobs
+    @jobs_with_dependency
+  end
+
+  private
+
+  # better do not call sort_with_no_significant_order and sort_jobs_with_dependency externally.
+  def sort_with_no_significant_order
+    @queue.sort! {|a,b| a<=>b }
+  end
+
+  def sort_jobs_with_dependency
+    sorted_stack = []
+    @jobs_with_dependency.each { |job, related_job|
+      # this is my idea:
+      # check for one free job and add it on stack starting from the position.
+      # then, if related exists, search it in the next_jobs
+      # depend_job => related_job
+      # this will work only with one dependency
+      if related_job == ''
+        sorted_stack << job
+      else
+        sorted_stack << related_job << job
+      end
+
+    }
+
+    @jobs_with_dependency = sorted_stack.uniq
+      # a => c
+      # b => a
+      # c => a
+      # this is wrong.
+
+      # a => b
+      # b => c
+      # c => f
+      # d =>
+      # f => d
+      # the correct sequence is:
+      # d f c b a       
+
+      # 1 all the jobs free from dependencies can go first.
+      # 2 all the dependent jobs can go back
+      # 3 all the jobs related to a dependency can go in the middle 
+      # d first
+      # we can liberate the job related to "d"
+      # f
+      # now we can liberate job c that's the job related to f
+      # c
+      # now we can liberate job b that's the job related to c
+      # b
+      # now we can liberate job a that's the job related to c
+      # a
+      # the correct sequence is:
+      # d f c b a
+
+      # a => b
+      # b => c
+      # c => f
+      # d => a
+      # f => c
+      # this is wrong:
+      #   so when all the existing job have a dependency there is something of bad.
+      # 
+
+      # a => b
+      # b => a
+      # c => f
+      # d =>
+      # f => c
+      # this is wrong:
+      #   there is a dead lock a job depends from a job that depend from the first
+      # 
+
+      # this is the example proposed from test:
+      # a =>
+      # b => c
+      # c => f
+      # d => a
+      # e => b
+      # f =>
+      #
+      # a first
+      # we can liberate the job d related to a
+      # d
+      # job d has not children, going to run the next free job
+      # there is a free job? no => (error), yes f
+      # f
+      # now we can liberate job c that's the job related to f
+      # c
+      # now we can liberate job b that's the job related to c
+      # b
+      # now we can liberate job e that's the job related to b
+      # e
+
   end
 
 end
--- spec/jobs_stack_spec.rb
+++ spec/jobs_stack_spec.rb
@@ -11,7 +11,7 @@ describe "JobsStack" do
 
       it 'return an empty sequence' do
         jobs_in_queue = JobsStack.new('')
-        expect(jobs_in_queue.queue).to eq [] # sequence => [] not ''
+        expect(jobs_in_queue.queue).to eq []
       end
 
     end
@@ -38,7 +38,45 @@ describe "JobsStack" do
         expect(jobs_stack.queue).to eq ['a','b','c']
       end
 
-      # three is the perfect number, but not in our case :)
+    end
+
+  end
+
+  context 'then, more complex cases' do
+
+    context 'with one dependency' do
+      context 'and with three jobs' do
+
+        let(:jobs_stack) { JobsStack.new("a =>\nb => c\nc =>a\n") }
+
+        it 'have a sequence sorted with a at first position, then c, before b' do
+          expect(jobs_stack.sorted_jobs).to eq ['a','c','b']
+        end
+        it 'have a sequence sorted alphatecally' do
+          expect(jobs_stack.queue).to eq ['a','b','c']
+        end
+
+      end
+
+      context 'and with four jobs' do
+        let(:jobs_stack) { JobsStack.new("a =>\nb => c\nc =>\nd => \n") }
+
+        it 'have this sorted sequence [a,c,b,d]' do
+          expect(jobs_stack.sorted_jobs).to eq ['a','c','b','d']
+        end
+        it 'have this sequence sorted alphatecally: [a,b,c,d]' do
+          expect(jobs_stack.queue).to eq ['a','b','c','d']
+        end
+
+      end
+
+    end
+
+    context 'with two dependencies' do
+
+    end
+
+    context 'with a lot of dependencies' do
 
     end
 
@@ -49,27 +87,27 @@ describe "JobsStack" do
     context 'with a not well-formed job' do
 
       it 'wrong symbol for dependency' do
-        skip
+        # skip
       end
 
       it 'wrong symbol for dependent job' do
-        skip
+        # skip
       end
 
       it 'wrong symbol for prior job' do
-        skip
+        # skip
       end
 
     end
 
     it 'dead lock dependency (when a job depends from itself)' do
-      skip
+      # skip
     end
 
     it 'circular dependencies' do
       # wow!
       # let's see after
-      skip
+      # skip
     end
 
   end
